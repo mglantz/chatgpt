d2508ded67d16be0305ab4325bfd421bcc9d5c77
Magnus Glantz <sudo@redhat.com>
2023-01-11 12:48:00 +0100

    Create gpt.py

---
+++ gpt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Chat GPT integration
+# Magnus Glantz, sudo@redhat.com, 2023
+# https://github.com/mglantz/chatgpt/
+
+import os
+import openai
+import sys
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+gpt_prompt = sys.argv[1]
+
+response = openai.Completion.create(
+  engine="text-davinci-003",
+  prompt=gpt_prompt,
+  temperature=0.5,
+  max_tokens=256,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+print(response['choices'][0]['text'])
